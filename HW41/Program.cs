c4c1fa9b712e6d7099f3171a42c715775371a63f
Roman Linkov <roman.linkov@vk.com>
2023-02-28 22:38:24 +0300

    Задача 41 решена

---
+++ HW41/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.numbersFromUser
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int m = NumberInput("Количество вводимых чисел M: ");
+
+int[] arrayFromUser = ArrayIntInput(m);
+
+int countPositiveNumbers=CountPositiveNumbers(arrayFromUser);
+
+PrintArrayInt(arrayFromUser);
+Console.Write($" -> {countPositiveNumbers}");
+Console.WriteLine();
+
+
+int CountPositiveNumbers(int[] array1)
+{
+    int resulst=0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i]>0) resulst++;
+    }
+    return resulst;
+}
+
+int[] ArrayIntInput(int m)
+{
+    int[] array = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        array[i]=NumberInput($"Введите число {i+1}: ");
+    }
+    return array;
+}
+
+void PrintArrayInt(int[] a)
+{
+    
+    for (int i=0; i<a.Length;i++)
+    {
+        Console.Write($"{a[i]}");
+        if (i<a.Length-1)Console.Write($", ");
+    }   
+}
+
+int NumberInput(string msg)
+
+{
+    System.Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
